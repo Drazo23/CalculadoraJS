9c302847ab32c085fd72b6cf0c99f03e7fe688fd
Daiana Erazo <daianaerazo@gmail.com>
2022-11-17 18:31:38 -0300

    no se puede dividir por cero

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,28 @@
-let numero1 = parseInt(prompt("ingrese un numero"));
+let numero1 = parseInt(prompt("Ingrese un numero"));
 let operacion = prompt ("Ingresa una operacion +, -, *, /");
-let numero2 = parseInt(prompt("ingrese un numero"));
-
+let numero2 = parseInt(prompt("Ingrese un numero"));
 let mensaje;
-switch (operacion){
-    case "+":
-        mensaje= (numero1 + numero2);
-        break;
-    case "*":
-        mensaje= (numero1*numero2);
-        break;
-    case "/":
-        mensaje= (numero1/numero2);
-        break;
-    case "-":
-        mensaje= (numero1-numero2);
-        break;
-    default:
-        mensaje= "ingrese una opcion correcta"
-        break;
+if(operacion=== "/" && numero1 ===0 || numero2===0){
+    alert("No se puede dividir por 0")
+}else{if(operacion!= "+, -, /, *"){
+    alert("Ingrese un operador valido entre +, -, *, / para optener un resultado valido")
+    }else{
+        switch (operacion){
+            case "+":
+                mensaje= (numero1 + numero2);
+                break;
+            case "*":
+                mensaje= (numero1*numero2);
+                break;
+            case "/":
+                mensaje= (numero1/numero2);
+                break;
+            case "-":
+                mensaje= (numero1-numero2);
+                break;             
+        }
+    }
 }
 
-alert (mensaje);
+
+alert ("Su resultado es" + " " + mensaje);
