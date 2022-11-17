e3f6a0b6848d11ea527a3d409965ba72984426fa
Daiana Erazo <daianaerazo@gmail.com>
2022-11-16 21:22:09 -0300

    falta mensaje de alerta

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+let numero1 = parseInt(prompt("ingrese un numero"));
+let operacion = prompt ("Ingresa una operacion +, -, *, /");
+let numero2 = parseInt(prompt("ingrese un numero"));
+
+let mensaje;
+switch (operacion){
+    case "+":
+        mensaje= (numero1 + numero2);
+        break;
+    case "*":
+        mensaje= (numero1*numero2);
+        break;
+    case "/":
+        mensaje= (numero1/numero2);
+        break;
+    case "-":
+        mensaje= (numero1-numero2);
+        break;
+    default:
+        mensaje= "ingrese un operador adecuado"
+        break;
+}
+
+alert (mensaje);
